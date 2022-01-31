b7f515ad5d1a543109d76fa9a1b65b091bce4705
Samuel <kungusamuel64@gmail.com>
2022-02-01 00:33:54 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Qrcode from './Components/Qrcode';
 import Nft from './Components/Nft';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Qrcode />
       <Nft />
     </div>
   );
--- src/Components/Nft.js
+++ src/Components/Nft.js
@@ -10,37 +10,37 @@ import img5 from './images/image-avatar.png';
 
  function Nft() {
      return (
-    <div className="nft">
-      <div class="container">
-        <div class="wrapper">
-              <div class="img-top">
-                    <img src={"img1"} alt=""/>
-                <div class="img-overlay">
-                    <img src={"img2"} alt=""/>
-                </div>
-              </div>
-            <div class="box-text">
-                <a href="">Equilibrium #3429</a>
-                <p>Our Equilibrium collection promotes balance and calm.</p>
-              <div class="flex">
-                <div class="eth">
+<div className="nft">
+  <div class="container">
+      <div class="wrapper">
+          <div class="img-top">
+              <img src={img1} alt=""/>
+            <div class="img-overlay">
+              <img src={img2} alt=""/>
+            </div>
+          </div>
+        <div class="box-text">
+              <a href="">Equilibrium #3429</a>
+              <p>Our Equilibrium collection promotes balance and calm.</p>
+          <div class="flex">
+              <div class="eth">
                   <img src={img3} alt=""/>
                   <p>0.041 ETH</p>
                 </div>
               <div class="days">
-                  <img src={img4} alt=""/>
-                  <p>3 days left</p>
-                  <hr/>
-              <div class="author">
-                <img src={"img5"} alt=""/>
-                <p>Creation of <a href="#" class="name">Jules Wyvern</a></p>
+                <img src={img4} alt=""/>
+                <p>3 days left</p>
+                <hr/>
+                <div class="author">
+                  <img src={img5} alt=""/>
+                    <p>Creation of <a href="#" class="name">Jules Wyvern</a></p>
                 </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
      );
 
  }
