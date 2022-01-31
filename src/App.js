469d66a19044f4ec474d9c154966d116d0b7f684
Samuel <kungusamuel64@gmail.com>
2022-02-01 00:20:18 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Qrcode from './Components/Qrcode.js';
+
+import Nft from './Components/Nft';
 
 
 function App() {
   return (
     <div className="App">
-      <Qrcode />
       
+      <Nft />
     </div>
   );
 }
--- src/Components/Nft.js
+++ src/Components/Nft.js
@@ -1,38 +1,45 @@
 import react from "react";
 import './Nft.css';
+import img1 from './images/image-equilibrium.jpg';
+import img2 from './images/icon-view.svg';
+import img3 from './images/icon-ethereum.svg';
+import img4 from './images/icon-clock.svg';
+import img5 from './images/image-avatar.png';
 
 
 
  function Nft() {
      return (
     <div className="nft">
-            <div class="card">
-                <div class="image">
-                    <img src="images/image-equilibrium.jpg" alt="" class="mainImg" />
-                <div>
-                <img src="images/icon-view.svg" alt="" />
+      <div class="container">
+        <div class="wrapper">
+              <div class="img-top">
+                    <img src={"img1"} alt=""/>
+                <div class="img-overlay">
+                    <img src={"img2"} alt=""/>
                 </div>
+              </div>
+            <div class="box-text">
+                <a href="">Equilibrium #3429</a>
+                <p>Our Equilibrium collection promotes balance and calm.</p>
+              <div class="flex">
+                <div class="eth">
+                  <img src={img3} alt=""/>
+                  <p>0.041 ETH</p>
                 </div>
-                <h3>Equilibrium #3429</h3>
-                <div class="description">
-        Our Equilibrium collection promotes balance and calm.
-      </div>
-      <div class="price-days">
-        <div class="price">
-          <img src="images/icon-ethereum.svg" alt="" />
-          0.041 ETH
+              <div class="days">
+                  <img src={img4} alt=""/>
+                  <p>3 days left</p>
+                  <hr/>
+              <div class="author">
+                <img src={"img5"} alt=""/>
+                <p>Creation of <a href="#" class="name">Jules Wyvern</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="days">
-          <img src="images/icon-clock.svg" alt="" />
-          3 days left
-        </div>
       </div>
-      <div class="creator">
-        <img src="images/image-avatar.png" alt="" width="30px" class="avatar" />
-
-        Creation of <span>Jules Wyvern</span>
-      </div>
-    </div>
     </div>
      );
 
